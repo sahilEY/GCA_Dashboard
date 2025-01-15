5111fa66cde149131419f11135b458f6e7359619
sahilEY <Sahil.Jain7@in.ey.com>
2025-01-15 15:34:13 +0530

    Update file

--- dashboard_app.py
+++ dashboard_app.py
@@ -72,36 +72,31 @@ with tab1:
 
     
 with tab2:
-    df = pd.read_excel("panel.xlsx")
+    df1 = pd.read_excel("panel.xlsx")
     st.subheader("Data Preview")
     st.dataframe(df.head())
-
-    # st.sidebar.header("Filters")
-    # account_filter = st.sidebar.selectbox("Select Skill Account", options=["All"] + list(df["Account"].unique()))
+    print (df1.columns)
+    st.sidebar.header("Panel Filter")
+    account_filter = st.sidebar.selectbox("Select Panel Account", options=["All"] + list(df1["Account"].unique()))
+    print (account_filter)
     # priority_filter = st.sidebar.selectbox("Select Skill Priority", options=["All"] + list(df["Priority"].unique()))
     # status_filter = st.sidebar.selectbox("Select Skill Status", options=["All"] + list(df["Status"].unique()))
     # # Apply filters
     # filtered_df = df.copy()
-    # if account_filter != "All":
-    #     filtered_df = filtered_df[filtered_df["Account"] == account_filter]
-    # if priority_filter != "All":
-    #     filtered_df = filtered_df[filtered_df["Priority"] == priority_filter]
-    # if status_filter != "All":
-    #     filtered_df = filtered_df[filtered_df["Status"] == status_filter]
-
-    # # Key metrics
-    # print (filtered_df)
-    # st.subheader("Key Metrics")
-
-    # total_positions = filtered_df["No of Positions"].sum()
+    filtered_df1 = df1.copy()
+    if account_filter != "All":
+        filtered_df1 = filtered_df1[filtered_df1["Panelist Name"] == account_filter]
+        total_panels = filtered_df1["Panelist Name"].nunique()
+    else:
+        total_panels = df1["Panelist Name"].nunique()
     # fulfilled_positions = filtered_df["Fulfilled"].sum()
     # net_open_positions = filtered_df["Net Open Positions"].sum()
     # conversion_rate = (fulfilled_positions / total_positions) * 100 if total_positions > 0 else 0
     
     # col1, col2, col3, col4 = st.columns(4)
-
-    # with col1:
-    #     st.metric("Total Positions", total_positions)
+    col5, col6, col7, col8 = st.columns(4)
+    with col5:
+         st.metric("Total Panel Member", total_panels)
 
     # with col2:
     #     st.metric("Fulfilled Positions", fulfilled_positions)
@@ -112,7 +107,7 @@ with tab2:
     # with col4:
     #     st.metric("Conversion Rate (%)", f"{conversion_rate:.2f}")
 with tab3:
-    df = pd.read_excel("skills_required.xlsx")
+    df3 = pd.read_excel("skills_required.xlsx")
     st.subheader("Data Preview")
-    st.dataframe(df.head())
+    st.dataframe(df3.head())
 
